b1436cfc5cfce619ddb26a6574421cd2750e034f
Paulstar200 <vitalispaul48@live.com>
2018-11-10 22:29:39 +0300

    [Feature #161861949] Update the login header

--- UI/static/js/login.js
+++ UI/static/js/login.js
@@ -1,37 +1,27 @@
-const uri = 'http://127.0.0.1:5000/api/v2/auth/login'
-
-
-let header = new Headers();
-header.append('Accept', 'application/json');
-header.append('Content-type', 'application/json');
-
+let loginUrl = 'https://storemanagerapi2.herokuapp.com//api/v2/auth/login';
 
 const login_form = document.getElementById('loginform');
-login_form.addEventListener('submit', loginFetch);
-
-let email = document.getElementById('email').value;
-let password = document.getElementById('password').value;
-
-let req = new Request(uri, {
-    method: 'POST',
-    headers: header,
-    body: JSON.stringify({email:email, password:password})
-})
 
 
-function loginFetch(e) {
-    e.preventDefault();
-    fetch(req)
-    .then(
-        (response) => response.json()
-        )
-    .then(
-        (data) => {
-            if (data.message === "Log in successful!")
-            {
-            window.location.href = "../owner.html";
-        }
+const loginUser = () => {
+    fetch(loginUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+      }),
+      headers: {
+        'Content-type' : 'application/json'
+      }
     })
-    .catch((err) => console.log(err)
-    )
-}
+    .then(response => response.json())
+    .then(loginData => {
+        if(loginData.message === "Log in successful!"){
+            localStorage.setItem('token', loginData.access_token);
+            window.location.href = "../UI/owner.html";
+        } else{
+            throw new Error(loginData.message);
+        }
+        console.log(loginData);
+    })
+  }
