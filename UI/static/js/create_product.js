07321d1864c550b0829aaaece43be1f9845283d3
Paulstar200 <vitalispaul48@live.com>
2018-11-11 00:22:32 +0300

    [Feature #161832854] Add create product javascript

--- UI/static/js/create_product.js
+++ UI/static/js/create_product.js
@@ -1,27 +1,30 @@
-let loginUrl = 'https://storemanagerapi2.herokuapp.com//api/v2/products';
+let createProductUrl = 'https://storemanagerapi2.herokuapp.com//api/v2/products';
 
-const login_form = document.getElementById('productform');
+const product_form = document.getElementById('productform');
 
 
-const loginUser = () => {
-    fetch(loginUrl, {
+const createProduct = () => {
+    fetch(createProductUrl, {
       method: 'POST',
       body: JSON.stringify({
-        email: document.getElementById('email').value,
-        password: document.getElementById('password').value
+        name: document.getElementById('createname').value,
+        inventory: document.getElementById('createinventory').value,
+        price: document.getElementById('createprice').value,
+        minimum_stock: document.getElementById('createstock').value,
+        category: document.getElementById('createcategory').value
       }),
       headers: {
         'Content-type' : 'application/json'
       }
     })
     .then(response => response.json())
-    .then(loginData => {
-        if(loginData.message === "Log in successful!"){
-            localStorage.setItem('token', loginData.access_token);
-            window.location.href = "../UI/owner.html";
+    .then(data => {
+        if(data.message === "Product created successfully"){
+            
+            window.location.href = "../UI/individual.html";
         } else{
-            throw new Error(loginData.message);
+            throw new Error(data.message);
         }
-        console.log(loginData);
+        console.log(data);
     })
   }
