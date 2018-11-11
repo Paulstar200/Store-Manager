30e22aa41e4e08115e69421ce86edaf3cf87f3c2
Paulstar200 <vitalispaul48@live.com>
2018-11-11 03:53:46 +0300

    [Feature #161832776] Update javascript file

--- UI/static/js/get_products.js
+++ UI/static/js/get_products.js
@@ -12,10 +12,29 @@ const getProducts = () => {
     .then(response => response.json())
     .then(data => {
         if(data.message === "Successfully retrieved products"){
-            window.location.href = "../UI/individual.html";
+            let ul =document.querySelector('#myproducts');
+            let df = new DocumentFragment();
+            data.forEach( (product) => {
+                let li = document.createElement('li');
+                let pn = document.createElement('p');
+                let pr = document.createElement('p');
+                let bn = document.createElement('button');
+                li.className = "individualprod";
+                bn.className = "prodbtn";
+                pn.textContent = ''.concat("Product: ", product.name);
+                pr.textContent = ''.concat("Price: ", product.price);
+                bn.textContent = "Add to cart";
+                li.appendChild(pn);
+                li.appendChild(pr);
+                li.appendChild(bn);
+                df.appendChild(li);
+            } )
+            ul.appendChild(df);
         } else{
             throw new Error(data.message);
         }
         console.log(data);
+    }).catch( (err) => {
+        console.log('ERROR:', err.message);
     })
   }
