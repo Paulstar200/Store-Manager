85ece743ed55c37ff09366dc1b321adeb7fcb36b
Paulstar200 <vitalispaul48@live.com>
2018-11-13 01:24:07 +0300

    [Feature #161832836] Update getproducts.js

--- UI/static/js/get_products.js
+++ UI/static/js/get_products.js
@@ -12,20 +12,32 @@ const getProducts = () => {
     .then(response => response.json())
     .then(data => {
         if(data['Products']){
-            let item = data['Products'][0];
-            console.log(item);
-            let ul = document.getElementById("myproducts");
-            let li = document.createElement('li');
-            let pn = document.createElement('p');
-            let pr = document.createElement('p');
-            let bt = document.createElement('button')
-            pn.innerHTML = item.name;
-            pr.innerHTML = item.price;
-            bt.innerHTML = "Add to cart";
-            li.append(pn);
-            li.append(pr);
-            li.append(bt);
-            ul.append(li);
+            var myArray = data['Products'];
+            console.log(myArray[0]);
+            for (let num = 0; num < myArray.length; num++) {
+                let item = myArray[num];
+                console.log(item);
+                let ul = document.getElementById("myproducts");
+                let li = document.createElement('li');
+                let pn = document.createElement('p');
+                let pr = document.createElement('p');
+                let bt = document.createElement('button');
+                
+                pn.innerHTML = "".concat("Product: ", item.name);
+                pr.innerHTML = "".concat("Price: ", item.price);
+                bt.innerHTML = "Add to cart";
+
+                pr.classList.add("productdescription")
+                pn.classList.add("productdescription")
+                
+                bt.classList.add('add-cart');
+                li.append(pn);
+                li.append(pr);
+                li.append(bt);
+                
+                ul.append(li);
+            }
+ 
         } else{
             document.getElementById('error-prod').innerHTML = "No products yet";
         }
