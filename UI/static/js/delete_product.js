579ecb530e8ea2e3deec122ac8727da294ba7d78
Paulstar200 <vitalispaul48@live.com>
2018-11-13 02:15:31 +0300

    [Feature #161832836] Implement modify and delete products

--- UI/static/js/delete_product.js
+++ UI/static/js/delete_product.js
@@ -1,7 +1,7 @@
 const deleteproduct = (productId) => {
     let confirmation = confirm("Are you sure you want to delete this entry?");
     if(confirmation == true){
-      fetch( `https://storemanagerapi2.herokuapp.com/api/v2/products/${productId}`, {
+      fetch( `https://storemanagerapi2.herokuapp.com/api/v2/products`, {
         method: 'DELETE',
         headers: {
           'Authorization' : `Bearer ${localStorage.getItem("token")}`,
@@ -18,3 +18,63 @@ const deleteproduct = (productId) => {
       })
     }
   }
+
+  window.onload = () => {
+    fetch("https://storemanagerapi2.herokuapp.com/api/v2/products", {
+        headers: {
+          'Authorization' : `Bearer ${localStorage.getItem("token")}`,
+          'Content-type' : 'application/json;'
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+            if (data) {
+                let output = `<tr>
+                <th>
+                    Id
+                </th>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Category
+                </th>
+                <th>
+                    Inventory
+                </th>
+                <th>
+                    Min stock
+                </th>
+                <th>
+                    Price
+                </th>
+                
+                <th>
+                </th>
+                <th>
+                </th>
+            </tr>`;
+            let myArray = data['Products']
+                myArray.forEach(product => {
+                    output += `
+                    <tr>
+                        <td>${product.id}</td>
+                        <td>${product.name}</td>
+                        <td>${product.category}</td>
+                        <td>${product.inventory}</td>
+                        <td>${product.minimum_stock}</td>
+                        <td>${product.price}</td>
+                        
+                        <td><button class="button" onclick = "openUpdateSection(${product.id})"><i class="fa fa-edit"></i></button></td>
+                        <td><button class="button" onclick = "deleteProduct(${product.id})"><i class="fa fa-trash"></i></button></td>
+                    </tr>
+                `
+                    document.getElementById("t2").innerHTML = output;
+                });
+                localStorage.setItem("allproducts", JSON.stringify(data['Products']))
+            }
+            else {
+                alert("No products to fetch");
+            }
+        });
+}
