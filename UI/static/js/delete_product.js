4484222af10d5780abe9618b5a3563bf33881865
Paulstar200 <vitalispaul48@live.com>
2018-11-13 11:41:06 +0300

    [Feature #161832836] Update update products

--- UI/static/js/delete_product.js
+++ UI/static/js/delete_product.js
@@ -8,6 +8,7 @@
         .then(response => response.json())
         .then(data => {
             if (data) {
+                console.log(data);
                 let output = `<tr>
                 <th>Id</th>
                 <th>Name</th>
@@ -16,35 +17,32 @@
                 <th>Min stock</th>
                 <th>Price</th>
                 <th>Delete</th>
-                <th>Modify</th>
+                <th>Update</th>
             </tr>`;
-            let myArray = data['Products']
-            //console.log(data['Products']);
-                myArray.forEach(product => {
-                  //console.log(product)
-                  let prod_id = product["product id"];
-                    let prod_num = parseInt(prod_id, 10);
-                    console.log(prod_num);
-                    output += `
-                    <tr>
-                        <td>${product["product id"]}</td>
-                        <td>${product.name}</td>
-                        <td>${product.category}</td>
-                        <td>${product.inventory}</td>
-                        <td>${product.minimum_stock}</td>
-                        <td>${product.price}</td>
-                        <td><button class="delete-btn" onclick = "deleteProduct(${prod_num})">Delete</button></td>
-                        <td><button class="modify-btn" onclick = "updateProd(${prod_num})">Modify</button></td>
-                    </tr>
-                ` 
-                
-                    document.getElementById("t2").innerHTML = output;
-                });
-                localStorage.setItem("allproducts", JSON.stringify(data['Products']))
-            }
-            else {
-                alert("No products yet");
-            }
+            let myArray = data['Products'];
+            myArray.forEach(product => {
+            let prod_id = product["product id"];
+            let prod_num = parseInt(prod_id, 10);
+            console.log(prod_num);
+            output += `
+            <tr>
+                <td>${product["product id"]}</td>
+                <td>${product.name}</td>
+                <td>${product.category}</td>
+                <td>${product.inventory}</td>
+                <td>${product.minimum_stock}</td>
+                <td>${product.price}</td>
+                <td><button class="delete-btn" onclick = "deleteProduct(${prod_num})">Delete</button></td>
+                <td><button class="modify-btn" onclick = "productUpdater(${prod_num})">Update</button></td>
+            </tr>
+        ` 
+            document.getElementById("t2").innerHTML = output;
+        });
+        localStorage.setItem("allproducts", JSON.stringify(data['Products']))
+        }
+        else {
+            alert("No products");
+        }
         });
 }
 
@@ -101,8 +99,9 @@ function updateProduct(e) {
               //console.log(data[0]);
               let errormessage = document.getElementById("update-error-msg");
               errormessage.innerHTML = '';
-              errormessage.innerHTML = data["message"];
-              if (data['message'] == "Product updated successfully!"){
+              errormessage.innerHTML = "<p>Insert proper values</p>";
+              console.log(errormessage);
+              if (data['message'] === "Product updated successfully!"){
                 window.location.reload();
               }
           })
@@ -110,16 +109,15 @@ function updateProduct(e) {
   }
 }
 
-function updateProd(productId) {
-  
+function productUpdater(productId) {
   let product_Id = productId;
   localStorage.setItem("productId", product_Id)
   fetch(`https://storemanagerapi2.herokuapp.com/api/v2/products/` + product_Id, {
       headers: {
-          'Authorization': localStorage.getItem("token")
+          'Authorization': `Bearer ${localStorage.getItem("token")}`
       }
   })
-      .then(res => res.json())
+      .then(response => response.json())
       .then(data => {
         let item = data['product'];
           document.getElementById("form-name").value = item.name;
