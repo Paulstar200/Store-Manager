4e5878955aaf3a36c89e74e7b6d522b196d3a217
Paulstar200 <vitalispaul48@live.com>
2018-11-13 21:21:42 +0300

    [Bug #161932457] Update CSS for products and navigation

--- UI/static/js/get_attendants.js
+++ UI/static/js/get_attendants.js
@@ -45,29 +45,6 @@ const getAttendants = () => {
 
                 ul.append(li);
             }
-            /* for (let num = 0; num < myArray.length; num++) {
-                let item = myArray[num];
-                console.log(item);
-                let ul = document.getElementById("myproducts");
-                let li = document.createElement('li');
-                let pn = document.createElement('p');
-                let pr = document.createElement('p');
-                let bt = document.createElement('button');
-                
-                pn.innerHTML = "".concat("Product: ", item.name);
-                pr.innerHTML = "".concat("Price: ", item.price);
-                bt.innerHTML = "Add to cart";
-
-                pr.classList.add("productdescription")
-                pn.classList.add("productdescription")
-                
-                bt.classList.add('add-cart');
-                li.append(pn);
-                li.append(pr);
-                li.append(bt);
-                
-                ul.append(li); 
-            } */
  
         } else{
             document.getElementById('error-prod').innerHTML = "No products yet";
