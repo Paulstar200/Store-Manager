3a032634ab998b006d3ccb1f14f8f818fff1ee64
Paulstar200 <vitalispaul48@live.com>
2018-11-14 15:14:53 +0300

    [Feature #161832788] Consume sales enpoint

---
+++ UI/static/js/sell_product.js
@@ -0,0 +1,30 @@
+let sellProductUrl = 'https://storemanagerapi2.herokuapp.com/api/v2/sales';
+
+const product_form = document.getElementById('sale-form');
+
+const sellProduct = () => {
+    
+    fetch(sellProductUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: document.getElementById('salename').value,
+        quantity: document.getElementById('salequantity').value
+      }),
+      headers: {
+        'Authorization' : `Bearer ${localStorage.getItem("token")}`,
+        'Content-type' : 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.message === "Sale successful"){
+            alert("Sale successful");
+            window.location.href = "../UI/sell_product.html";
+            localStorage.setItem("id", data.SalesModel.id);
+            document.getElementById('sell-create').innerHTML = data.message;
+        } else{
+            document.getElementById('error-sale').innerHTML = data.message;
+        }
+        console.log(data);
+    })
+  }
