0f62cf15a9756b484da1d3c2644a174e38421d18
Paulstar200 <vitalispaul48@live.com>
2018-11-17 01:15:07 +0300

    [Feature #161932246] Fix the sales records table

--- UI/static/js/sales_records.js
+++ UI/static/js/sales_records.js
@@ -12,23 +12,39 @@ const salesRecords = () => {
     .then(data => {
         if(data.message === "Successfully retrieved the sales records"){
             let i;
-            for (i = 0; i < data; i++) {
-                document.getElementbyId("adminview").innerHTML += `
-                <tr>
-                    <td>"${data[i].id}"</td>
-                    <td>"${data[i].product_name}"</td>
-                    <td>"${data[i].quantity}"</td>
-                    <td>"${data[i].price}"</td>
-                    <td>"${data[i].total_price}"</td>
-                    <td>"${data[i].sold_by}"</td>
-                </tr>
-                 `
-            }
-        } else{
+            console.log(data);
+            let output = `<tr>
+                <th>Sale id</th>
+                <th>Product name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Total price</th>
+                <th>Sold by</th>
+            </tr>`;
+
+            let myArray = data['Sales'];
+            myArray.forEach(sale => {
+            let sale_id = sale["sale id"];
+            output += `
+            <tr>
+                <td>${sale_id}</td>
+                <td>${sale.product_name}</td>
+                <td>${sale.quantity}</td>
+                <td>${sale.price}</td>
+                <td>${sale.total_price}</td>
+                <td>${sale.attendant_name}</td>
+            </tr>
+        ` 
+            document.getElementById("adminview").innerHTML = output;
+        }) 
+            localStorage.setItem("allsales", JSON.stringify(data['Sales']));
+            var elem = document.getElementById("nosales");
+            elem.remove();
+    }
+        else{
             let table = document.getElementById('adminview');
             table.style.display = "none";
             document.getElementById('nosales').innerHTML = data.message;
-           
         }
         console.log(data);
     })
